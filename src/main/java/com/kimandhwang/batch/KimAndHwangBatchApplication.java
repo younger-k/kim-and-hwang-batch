76683439f71fc38cc5f2d87d7e956d70bd3f373f
younger-k <younger9403@gmail.com>
2021-10-24 01:01:34 +0900

    spring initialize

---
+++ src/main/java/com/kimandhwang/batch/KimAndHwangBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kimandhwang.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimAndHwangBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KimAndHwangBatchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kimandhwang/batch/KimAndHwangBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimandhwang.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimAndHwangBatchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
